d335800188110727671503e3795050c6c9ce20c5
RenFukatsu <fukatsuren88@gmail.com>
2021-05-19 00:34:43 +0900

    format

--- math/prime_factor.h
+++ math/prime_factor.h
@@ -1,11 +1,11 @@
 #ifndef LIBRARY_MATH_PRIME_FACTOR_H_
 #define LIBRARY_MATH_PRIME_FACTOR_H_
 
-#include <vector>
 #include <map>
+#include <vector>
 
 namespace Math {
-template<typename T>
+template <typename T>
 std::map<T, int> factorize(T n) {
     std::map<T, int> res;
     for (T i = 2; i * i <= n; i++) {
